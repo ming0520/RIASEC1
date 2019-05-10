12135836e4fc6c60d9ae550fb957ee926741b90d
ming0520 <mingblog0520@gmail.com>
2019-05-11 01:58:53 +0800

    Create database.sql
    the basic command to startup database

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Users(
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username varchar(30) NOT NULL,
+    password varchar(60) NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    email varchar(60) NOT NULL
+    );
+
+    tanseb
+    sebtan
+
+    INSERT INTO users(username,password,first_name,last_name,email) 
+    VALUES ('admin','admin','Zhong Ming','Tan','admin@riasecqiup.tk')
