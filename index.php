3cecd8fd3d4f915378cc578e0b02466866674be1
ming0520 <mingblog0520@gmail.com>
2019-05-11 02:00:10 +0800

    Update index.php and question.php
    index.php -> login page
    question.php -> after login success

--- index.php
+++ index.php
@@ -1,86 +1,35 @@
-<?php 
-    include_once ("include/riasec.inc.php");
-    include_once ("include/process.inc.php");
-?>
+<?php
+    include_once("include/Dbh.inc.php");
+    include_once("include/User.inc.php");
+    $userDataArray = array();
+    if(isset($_POST['submit'])){
+        $userDataArray = array(
+            "username" => $_POST["username"],
+            "password" => $_POST["password"]
+        );
 
+        $user = new User();
+        $user->login($userDataArray);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>RIASEC</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/gradient_form.css">
 </head>
-<body class="container">
-    <div class="col">
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <div id="question-list">
-                <table border="1">
-                    <?php
-                        $question_number = 1;
-                        foreach ($question_list as $question){
-                                $string = "<tr>
-                                    <td>".$question_number . ") " . $question['question'] . "</td>".
-                                    "<td><input type='checkbox' name = 'check_answer[]' value = '" .$question['answer'] . "' ></td>".
-                                "</tr>";
-                                echo $string;
-                                $question_number++;                       
-                        } 
-                    ?>
-                </table>
-            </div>
-            <br>
-            <div class="control">
-                <button type="submit" name="submit">Submit</button>
-            </div>
+<body>
+    <div class="login">
+        <h1>Login</h1>
+        <p style="color = white;">username and password is admin, admin</p>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="text" name="username" placeholder="Username" required="required" />
+            <input type="password" name="password" placeholder="Password" required="required" />
+            <button type="submit" class="btn btn-primary btn-block btn-large" name="submit">Login</button>
         </form>
-    </div>
-
-    <div id="result" class="col">
-        <table border="1">
-            <thead>
-                <tr>
-                    <td>Category</td>
-                    <td>Number</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>R</td>
-                    <td><?php echo $r_count ?></td>
-                </tr>
-                <tr>
-                    <td>I</td>
-                    <td><?php echo $i_count ?></td>
-                </tr>
-                <tr>
-                    <td>A</td>
-                    <td><?php echo $a_count ?></td>
-                </tr>
-                <tr>
-                    <td>S</td>
-                    <td><?php echo $s_count ?></td>
-                </tr>
-                <tr>
-                    <td>E</td>
-                    <td><?php echo $e_count ?></td>
-                </tr>
-                <tr>
-                    <td>C</td>
-                    <td><?php echo $c_count ?></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+</div>
 </body>
 </html>
-
-<?php
-
-$r_count =
-    $i_count =
-    $a_count =
-    $s_count = 
-    $e_count =
-    $c_count = 0;
---
+++ question.php
@@ -0,0 +1,93 @@
+<?php 
+    include_once ("include/riasec.inc.php");
+    include_once ("include/process.inc.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>RIASEC</title>
+</head>
+<body class="container">
+    <center><h1>RIASEC Test</h1> </center>   
+    <div class="row">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="col">
+        <h1>Question</h1>
+            <div id="question-list">
+                <table border="1">
+                    <?php
+                        $question_number = 1;
+                        foreach ($question_list as $question){
+                                $string = "<tr>
+                                    <td>".$question_number . ") " . $question['question'] . "</td>".
+                                    "<td><input type='checkbox' name = 'check_answer[]' value = '" .$question['answer'] . "' ></td>".
+                                "</tr>";
+                                echo $string;
+                                $question_number++;                       
+                        } 
+                    ?>
+                </table>
+            </div>
+            <br>
+            <div class="control">
+                <button type="submit" name="submit">Submit</button>
+            </div>
+         </div>
+        <div id="result" class="col">
+        <h1>Result</h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <td>Category</td>
+                        <td>Number</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>R</td>
+                        <td><?php echo $r_count ?></td>
+                    </tr>
+                    <tr>
+                        <td>I</td>
+                        <td><?php echo $i_count ?></td>
+                    </tr>
+                    <tr>
+                        <td>A</td>
+                        <td><?php echo $a_count ?></td>
+                    </tr>
+                    <tr>
+                        <td>S</td>
+                        <td><?php echo $s_count ?></td>
+                    </tr>
+                    <tr>
+                        <td>E</td>
+                        <td><?php echo $e_count ?></td>
+                    </tr>
+                    <tr>
+                        <td>C</td>
+                        <td><?php echo $c_count ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </form>
+    </div>
+    
+
+    
+</body>
+</html>
+
+<?php
+
+$r_count =
+    $i_count =
+    $a_count =
+    $s_count = 
+    $e_count =
+    $c_count = 0;
