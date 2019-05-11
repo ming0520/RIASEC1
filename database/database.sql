aa049380ee8060c65927a36b6f77fca697828f3d
ming0520 <mingblog0520@gmail.com>
2019-05-11 11:52:07 +0800

    Update database.sql
    Add new table and coommand for reset id

--- database/database.sql
+++ database/database.sql
@@ -12,3 +12,17 @@ CREATE TABLE Users(
 
     INSERT INTO users(username,password,first_name,last_name,email) 
     VALUES ('admin','admin','Zhong Ming','Tan','admin@riasecqiup.tk')
+
+    CREATE TABLE riasec(
+        id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        username varchar(30) NOT NULL,
+        r_count int NOT NULL,
+        i_count int NOT NULL,
+        a_count int NOT NULL,
+        s_count int NOT NULL,
+        e_count int NOT NULL,
+        c_count int NOT NULL
+    );
+
+    ALTER TABLE users DROP COLUMN id
+    ALTER TABLE users ADD id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT
